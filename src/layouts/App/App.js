36b0162c44c8a613c4c20f7303a4aca8ef631bf0
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-02-15 00:35:34 +0100

    Create main component

---
+++ src/layouts/App/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from "react-router-dom";
+import "./App.css";
+import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
+import Page from "../Page/Page";
+import Footer from "../Footer/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <header>
+            <Header />
+          </header>
+          <main>
+            <aside>
+              <Nav />
+            </aside>
+            <section className="page">
+              <Page />
+            </section>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
