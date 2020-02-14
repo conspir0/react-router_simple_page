a9dcf925ed50c818ea4553f9fd2f131c0a3ccf10
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-02-15 00:06:39 +0100

    Structure is ready

---
+++ src/layouts/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import {} from "react-router-dom";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+export default App;
