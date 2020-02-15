584539832998770c8464479a24aa25f668b71fe0
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-02-15 21:33:57 +0100

    Create Nav layout

--- src/layouts/Nav/Nav.js
+++ src/layouts/Nav/Nav.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./nav.css";
+
+const list = [
+  {
+    name: "home",
+    path: "/",
+    exact: true
+  },
+  {
+    name: "products",
+    path: "/products",
+    exact: false
+  },
+  {
+    name: "contact",
+    path: "/contact",
+    exact: false
+  },
+  {
+    name: "admin",
+    path: "/admin",
+    exact: false
+  }
+];
 
 const Nav = props => {
-  return <div>NAV</div>;
+  const navList = list.map(item => {
+    return (
+      <li key={item.name}>
+        <NavLink to={item.path} exact={item.exact}>
+          {item.name}
+        </NavLink>
+      </li>
+    );
+  });
+
+  return (
+    <nav>
+      <ul>{navList}</ul>
+    </nav>
+  );
 };
 
 export default Nav;
