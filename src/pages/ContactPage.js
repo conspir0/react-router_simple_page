49d30fecdd2d046f11fb36c3e0e5263620958cd7
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2020-02-18 21:27:24 +0100

    Create Contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import { Prompt } from "react-router-dom";
+import "./ContactPage.css";
 
-const ContactPage = props => {
-  return <div>Contact Page</div>;
-};
+class ContactPage extends Component {
+  state = {
+    text: ""
+  };
+
+  handleTextChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.prevntDefault();
+
+    this.setState({
+      text: ""
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="contact">
+          <h1 className="contact__title">write to us</h1>
+          <form className="contact__form" onSubmit={this.handleSubmit}>
+            <textarea
+              value={this.state.text}
+              onChange={this.handleTextChange}
+              placeholder="Type here..."
+              className="form__textarea"
+            ></textarea>
+            <button className="form__btn form__btn--submit">Submit</button>
+          </form>
+          <Prompt
+            when={this.state.text.length > 0}
+            message="If you leave this page you will lose your message. Are you sure?"
+          ></Prompt>
+        </div>
+      </>
+    );
+  }
+}
 
 export default ContactPage;
